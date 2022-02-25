f97bfd701d47fc40581b7eb2cff12da861659ec8
johelder <johelder.araujo@pede.ai>
2022-02-24 23:19:51 -0300

    feat: adding home struct and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+          
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 };
 
 export default Home;
