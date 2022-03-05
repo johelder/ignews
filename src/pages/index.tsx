6714ffbf420263dfd1c9188a04c0d9c735b22bca
johelder <johelder.arj@gmail.com>
2022-03-05 13:16:50 -0300

    feat: using ssg to get product price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import type { NextPage } from "next";
+import type { GetStaticProps } from "next";
 import Head from "next/head";
+
 import { SubscribeButton } from "../components/SubscribeButton";
+
+import { stripe } from "../service/stripe";
 
 import styles from './home.module.scss';
 
-const Home: NextPage = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+const Home = ({ product }: HomeProps) => {
   return (
     <>
       <Head>
@@ -19,10 +29,10 @@ const Home: NextPage = () => {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -32,3 +42,23 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1KWtx7HR8YBNVXrPkwKh2GD3');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount! / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
+}
