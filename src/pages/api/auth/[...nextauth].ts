965d5c4159a75aeaa1920af5cfbfe5852226f77b
johelder <johelder.araujo@pede.ai>
2022-03-12 21:52:28 -0300

    fix: adjustment in NextAuth signIn callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,38 @@
-import NextAuth from "next-auth/next";
-import GitHubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
-    GitHubProvider({
+    GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user',
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean> {
+      const { email } = user;
 
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: {
+              email,
+            },
+          })
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
