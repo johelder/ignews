a6bf1bbaf3812dcca31467d6d8122f05074e3109
johelder <johelder.arj@gmail.com>
2022-04-05 19:28:38 -0300

    config: create prismic config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
-import {SessionProvider as NextAuthProvider, } from 'next-auth/react';
+import { SessionProvider as NextAuthProvider, } from 'next-auth/react';
+
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+
+import { linkResolver, repositoryName } from "../../prismicio";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider>
-      <Header />
-      <Component {...pageProps} />
-    </NextAuthProvider>
+    <PrismicProvider linkResolver={linkResolver} >
+      <PrismicPreview repositoryName={repositoryName}>
+        <NextAuthProvider>
+          <Header />
+          <Component {...pageProps} />
+        </NextAuthProvider>
+      </PrismicPreview>
+    </PrismicProvider>
+
   );
 }
 
